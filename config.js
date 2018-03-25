b1168a1dc146ab8f2167ba8f1c257076e64b84c6
sangeetmoy <35575581+sangeetmoy@users.noreply.github.com>
2018-03-25 11:22:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+/*jslint node: true */
+
+module.exports = {
+    "adminPass": "sangeetmoydas",
+    "serverURL": "http://localhost:8091/",
+    "serverDomain": "localhost",
+    "environment": "development",
+    "debug": true,
+    "dbDebug": true,
+    "defaultSettings": {}
+};
